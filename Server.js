5f655eae3bac5a504073d3a4332e8205ede37b0a
Kyle Steinle <kyledavid022@gmail.com>
2017-08-31 22:39:36 -0700

    modified Server.js

--- Server.js
+++ Server.js
@@ -1,6 +1,9 @@
 import express from 'express'
-const app = express()
+var app = express()
+var port = process.env.PORT || 8000
 
 app.use(express.static(__dirname + '/build'))
 
-app.listen(process.env.PORT || 8080)
+app.listen(port, function() {
+  console.log("App is running on port " + port)
+})
