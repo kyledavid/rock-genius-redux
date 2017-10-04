aa12bed8f96733433d71cce01c9f490f8b56423c
Kyle Steinle <kyledavid022@gmail.com>
2017-10-04 11:07:44 -0700

    added bg image to boulder selection

--- src/containers/BoulderSelection.js
+++ src/containers/BoulderSelection.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Link, Route } from 'react-router-dom'
+import bgImage from '../img/kraft-bg3.jpg'
 
 export default () => (
-  <div className="container">
-    <h1>Choose a Boulder</h1>
-    <ul className="route-list">
-      <li>
-        <Link to="/boulders/the-pearl" >The Pearl</Link>
-      </li>
-    </ul>
+  <div className="selection-wrapper" style={{backgroundImage: `url(${bgImage})`}}>
+    <div className="container">
+      <h1>Choose a Boulder</h1>
+      <ul className="route-list">
+        <li>
+          <Link to="/boulders/the-pearl" >The Pearl</Link>
+        </li>
+      </ul>
+    </div>
   </div>
 )
