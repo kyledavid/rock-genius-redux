19fd56128c62cbef09aa9cf8709f2946188560ea
Kyle Steinle <kyledavid022@gmail.com>
2017-11-08 19:42:19 -0800

    don't push giant images

--- src/index.js
+++ src/index.js
@@ -5,5 +5,11 @@ import 'normalize.css'
 import App from './containers/App'
 import './style.scss'
 import '../node_modules/font-awesome/css/font-awesome.min.css'
+import initialState from '../data-model.json'
+import storeFactory from './store'
 
 ReactDOM.render(<App />, document.getElementById('app'))
+
+const store = storeFactory(initialState)
+
+window.store = store
--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,14 @@
-import { createStore } from 'redux'
+import reducer from './reducers'
+import { applyMiddleware, createStore } from 'redux'
+
+const fakeMiddleware = store => next => action => {
+  let result
+
+  result = next(action)
+
+  return result
+}
+
+export default (initialState={}) => {
+  applyMiddleware(fakeMiddleware)(createStore)(reducer, initialState)
+}
