9822bd55f9ed936f8fea432681e4560a20bface6
Kyle Steinle <kyledavid022@gmail.com>
2017-11-30 12:46:53 -0800

    ping every few minutes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { BrowserRouter, Link, Route, withRouter } from 'react-router-dom'
 import BoulderSelection from './BoulderSelection'
 import BoulderDisplay from '../containers/BoulderDisplay'
 
-const App = () => (
-  <BrowserRouter>
-    <div>
-      <Route exact path="/" component={BoulderSelection} />
-      <Route path="/boulders/:boulderName/:routeName?" component={BoulderDisplay} />
-    </div>
-  </BrowserRouter>
-)
+class App extends Component {
+  componentWillMount() {
+    // ping the app every 5 minutes
+      setInterval(function() {
+          http.get("http://<your app name>.herokuapp.com");
+      }, 300000)
+  }
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={BoulderSelection} />
+          <Route path="/boulders/:boulderName/:routeName?" component={BoulderDisplay} />
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
 
 export default App
